8f3d8bba6f99fea28997d148439798d51a3cd3dd
Damian Gola <gola@imbs.uni-luebeck.de>
2018-11-14 14:15:34 +0100

    reformat

--- .batchtools.conf.R
+++ .batchtools.conf.R
@@ -1,5 +1,10 @@
-cluster.functions = batchtools::makeClusterFunctionsSlurm(template = "~/.batchtools.slurm.tmpl", nodename = "login01")
+cluster.functions = batchtools::makeClusterFunctionsSlurm(
+  template = "~/.batchtools.slurm.tmpl", 
+  nodename = "login01"
+)
 sleep = 3
-default.resources = list(ntasks = 1, ncpus = 1,
-                         memory = 6000, partition = "batch",
-                         chunks.as.arrayjobs = TRUE)
+default.resources = list(
+  ntasks = 1, ncpus = 1,
+  memory = 6000, partition = "batch",
+  chunks.as.arrayjobs = TRUE
+)
